7c0040e4eddd03fd48dc06d6e66b316c08b3ff92
AlaaskaBot <123285025+AlaaskaBot@users.noreply.github.com>
2025-05-10 18:47:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -67,7 +67,7 @@ async def give_filter(client: Client, message: Message):
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot: Client, message: Message):
     glob = await global_filters(bot, message)
-    wait_msg = await message.reply_text(f"<b>Searching For {message.text}...</b>", parse_mode=enums.ParseMode.HTML, quote=True)
+    wait_msg = await message.reply_text(f"<b>Searching For Your Query...</b>", parse_mode=enums.ParseMode.HTML, quote=True)
     await auto_filter(bot, message, wait_msg)
     if glob:
         await glob.delete()

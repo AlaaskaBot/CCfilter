ef20ed0a8d9dcfa5c9342dbd1002a7326d563fd4
AlaaskaBot <123285025+AlaaskaBot@users.noreply.github.com>
2025-05-27 07:10:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,21 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.autofilter import filter_query  # Make sure this is the correct path
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start_handler(client: Client, message: Message):
+    if len(message.command) > 1:
+        payload = message.command[1]
+        if payload.startswith("getfile-"):
+            keyword = payload.replace("getfile-", "").replace("-", " ")
+            message.text = keyword  # Send fake message to autofilter
+            return await filter_query(client, message)
+
+    await message.reply(
+        "Hello! I am your AutoFilter Bot.\n\n"
+        "Use /link <file name> to get a sharable link.",
+        quote=True
+    )
 
 @Client.on_message(filters.command("link") & filters.private)
 async def get_file_link(client: Client, message: Message):
@@ -20,19 +37,4 @@ async def get_file_link(client: Client, message: Message):
         disable_web_page_preview=True,
         quote=True
     )
-@Client.on_message(filters.command("start") & filters.private)
-async def start_handler(client: Client, message: Message):
-    if len(message.command) > 1:
-        payload = message.command[1]
-        if payload.startswith("getfile-"):
-            keyword = payload.replace("getfile-", "").replace("-", " ")
-            # Send this keyword to the filter search logic
-            from plugins.autofilter import filter_query
-            message.text = keyword
-            return await filter_query(client, message)
-
-    await message.reply(
-        "Hello! I am your AutoFilter Bot.\n\nUse `/link <file name>` to get a sharable link.",
-        quote=True
-    )
-  
+    

377636fc038802a993740c92fdc9d16f59eae287
AlaaskaBot <123285025+AlaaskaBot@users.noreply.github.com>
2025-05-27 07:38:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from filters import filter_query
+from pm_filter import filter_query
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handler(client: Client, message: Message):
